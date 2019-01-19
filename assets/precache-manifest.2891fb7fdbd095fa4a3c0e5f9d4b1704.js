591d6ea0e65be374d6fa955e90cffcd3bdf09471
Olawale Malomo <warlay234@gmail.com>
2019-01-19 17:41:30 +0100

    Wrap it react and sails together

---
+++ assets/precache-manifest.2891fb7fdbd095fa4a3c0e5f9d4b1704.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e76d8bb5d627c12afb34",
+    "url": "/static/js/main.e76d8bb5.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "e76d8bb5d627c12afb34",
+    "url": "/static/css/main.4e70e08d.chunk.css"
+  },
+  {
+    "revision": "46962946ae93a3515ae280aa351476a6",
+    "url": "/index.html"
+  }
+];
