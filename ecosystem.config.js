dbc0ce94217eb8de054619d9d5713ff9237d65c0
Olawale Malomo <warlay234@gmail.com>
2019-01-21 02:58:23 +0100

    Update config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-52-34-109-198.us-west-2.compute.amazonaws.com',
+      host: 'ec2-35-164-53-114.us-west-2.compute.amazonaws.com',
       key: '~/.ssh/rauxa.pem',
       ref: 'origin/master',
       repo: 'https://github.com/kingstuffy/rauxa-backend-code-challenge.git',
