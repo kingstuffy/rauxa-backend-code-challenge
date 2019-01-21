7dd30768d3e2926ed7ccd4b0b4efcaa2d8a8af4f
Olawale Malomo <warlay234@gmail.com>
2019-01-21 03:48:00 +0100

    Fetch contacts from API

--- assets/react/src/scenes/Dashboard/scenes/Contacts/scenes/ContactsHome/components/ContactList/index.js
+++ assets/react/src/scenes/Dashboard/scenes/Contacts/scenes/ContactsHome/components/ContactList/index.js
@@ -12,9 +12,11 @@ const ContactList = (props) => {
       <Table responsive>
         <thead>
         <tr>
+          <th className="contact-list__th">SN</th>
           <th className="contact-list__th contact-list__th--avatar">Avatar</th>
           <th className="contact-list__th">Name</th>
           <th className="contact-list__th">Phone number</th>
+          <th className="contact-list__th">Created</th>
         </tr>
         </thead>
         <tbody>
@@ -22,11 +24,17 @@ const ContactList = (props) => {
           data.map((item, index) =>
             (
               <tr key={index}>
+                <td className="contact-list__td">{item.id}</td>
                 <td className="contact-list__td">
                   <img className="contact-list__img" src={item.imgUrl}/>
                 </td>
                 <td className="contact-list__td">{item.name}</td>
                 <td className="contact-list__td">{item.phone}</td>
+                <td className="contact-list__td">
+                  {new Date(item.createdAt).toDateString()},
+                  {' '}
+                  {new Date(item.createdAt).toTimeString()}
+                </td>
               </tr>
             ))
         }
