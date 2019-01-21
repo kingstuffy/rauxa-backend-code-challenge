0609e67d58298a91f0e3a34552f8f34c932ca785
Olawale Malomo <warlay234@gmail.com>
2019-01-21 01:10:58 +0100

    Update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -9,7 +9,7 @@ module.exports = {
       host: 'ec2-52-34-109-198.us-west-2.compute.amazonaws.com',
       key: '~/.ssh/rauxa.pem',
       ref: 'origin/react-app-setup',
-      repo: 'github.com/kingstuffy/rauxa-backend-code-challenge.git',
+      repo: 'https://github.com/kingstuffy/rauxa-backend-code-challenge.git',
       path: '/home/ubuntu/rauxa-backend-code-challenge',
       'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
     }
